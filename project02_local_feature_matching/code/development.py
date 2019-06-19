d5b268fc0e5c41f1659b348badb611f3e041fa45
sebastianbertoli <bertoli.sebastian@gmail.com>
2019-06-19 21:00:38 +0200

    format document

--- project02_local_feature_matching/code/development.py
+++ project02_local_feature_matching/code/development.py
@@ -3,6 +3,7 @@ from scipy.spatial.distance import cdist
 from skimage.feature import peak_local_max
 from skimage import filters
 
+
 def get_idx_2smallest_dist(distances):
     """Given a distance matrix it returns the index of the k
     smallest distances.
@@ -14,7 +15,7 @@ def get_idx_2smallest_dist(distances):
 
     Returns
     -------
-    
+
     d1_ix, d2_ix: Tuple.
         For eact row of the distance matrix it returns the index of the smallest
         and second smallest distance.
@@ -22,33 +23,34 @@ def get_idx_2smallest_dist(distances):
     """
     n_rows = distances.shape[0]
     idxs = np.argsort(distances, axis=1)[:, :2]
-    d1_ix = [np.arange(n_rows), idxs[:,0]]
-    d2_ix = [np.arange(n_rows), idxs[:,1]]
+    d1_ix = [np.arange(n_rows), idxs[:, 0]]
+    d2_ix = [np.arange(n_rows), idxs[:, 1]]
     return d1_ix, d2_ix
+
 
 def match_features(im1_features, im2_features):
     """ Returns the index of the common image features as well as the 
     confidence of a correct match.
-    
+
     Parameters
     ----------
-    
+
     im1_features: array, features of image 1.
-    
+
     im2_features: array, features of image 2.
-    
+
     Returns
     -------
-    
+
     matches: array
         First column = matches for image 1.
         Second column = matches for image 2.
-    
+
     confidences: list, confidence for each match identified. 
         We define it as:
 
             confidence = 1 - NNDR
-        
+
         where:
 
         NDDR = \frac{d_1}{d_2} = \frac{||D_A - D_B||}{||D_A - D_C||}
@@ -61,13 +63,14 @@ def match_features(im1_features, im2_features):
 
     matches = np.stack((d1_ix[0], d1_ix[1]), axis=1)
     confidences = 1 - nddr
-    
+
     return matches, confidences
+
 
 def get_features(image, x, y, feature_width):
     '''
     Returns a set of feature descriptors for a given set of interest points.
-    
+
     Parameters
     ----------
 
@@ -94,36 +97,42 @@ def get_features(image, x, y, feature_width):
     offset = feature_width // 2
     descriptors = list()
     n_bins = 8
-    
+
     # compute gradients, magnitudes, angles
     gradients = np.array(np.gradient(image))
     magnitudes = np.linalg.norm(gradients, axis=0)
     angles = np.angle(np.arctan2(gradients[0], gradients[1]), deg=True)
-    
+
     # Loop through keypoints
-    for xi, yi in zip(x,y):
+    for xi, yi in zip(x, y):
         #crop_image = image[yi-offset+1:yi+offset+1, xi-offset+1:xi+offset+1]
-        crop_magnitudes = magnitudes[yi-offset+1:yi+offset+1, xi-offset+1:xi+offset+1]
+        crop_magnitudes = magnitudes[yi-offset +
+                                     1:yi+offset+1, xi-offset+1:xi+offset+1]
         crop_angles = angles[yi-offset+1:yi+offset+1, xi-offset+1:xi+offset+1]
         if crop_magnitudes.shape != (feature_width, feature_width):
             # Crop does not satisfy size constraint, skip keypoint.
             continue
 
         # Create SIFT descriptor
-        patches_magnitudes = np.array(np.hsplit(np.array(np.hsplit(crop_magnitudes, 4)).reshape(4,-1),4)).reshape(-1,feature_width)
-        patches_angles = np.array(np.hsplit(np.array(np.hsplit(crop_angles, 4)).reshape(4,-1),4)).reshape(-1,feature_width)
+        patches_magnitudes = np.array(np.hsplit(np.array(
+            np.hsplit(crop_magnitudes, 4)).reshape(4, -1), 4)).reshape(-1, feature_width)
+        patches_angles = np.array(np.hsplit(np.array(
+            np.hsplit(crop_angles, 4)).reshape(4, -1), 4)).reshape(-1, feature_width)
         feature_vector = list()
         for patch_i in range(patches_magnitudes.shape[0]):
-            bins = np.digitize(patches_angles[patch_i], np.arange(0,360,360 // n_bins))
+            bins = np.digitize(
+                patches_angles[patch_i], np.arange(0, 360, 360 // n_bins))
             bin_vector = np.zeros(n_bins)
             for bin_i in range(0, n_bins):
                 mask = np.array(bins == bin_i).flatten()
-                bin_vector[bin_i] = np.sum(patches_magnitudes[patch_i].flatten()[mask])
+                bin_vector[bin_i] = np.sum(
+                    patches_magnitudes[patch_i].flatten()[mask])
             feature_vector.append(bin_vector)
-        feature_vector_norm = (np.array(feature_vector).flatten() / 
+        feature_vector_norm = (np.array(feature_vector).flatten() /
                                np.array(feature_vector).flatten().sum())
         descriptors.append(feature_vector_norm)
     return np.array(descriptors)
+
 
 def get_interest_points(image, feature_width, sigma=2, k=0.04):
     '''
@@ -156,7 +165,7 @@ def get_interest_points(image, feature_width, sigma=2, k=0.04):
     4.  Compute a scalar interest measure using one of the formulas discussed above.
     5.  Find local maxima above a certain threshold and report them as detected 
         feature point locations.
-    ''' 
+    '''
     image_blurred = filters.gaussian(image, sigma)
     Iy, Ix = np.gradient(image_blurred)
     Ixx = filters.gaussian(Ix * Ix, sigma)
@@ -166,11 +175,10 @@ def get_interest_points(image, feature_width, sigma=2, k=0.04):
     R_norm = (R-np.min(R))/(np.max(R)-np.min(R))
     corners = R_norm
     threshold = np.mean(R_norm)
-    mask = [R_norm<threshold]
+    mask = [R_norm < threshold]
     corners[mask] = 0
-    keypoints = peak_local_max(corners, threshold_rel=0.2, exclude_border=True, 
+    keypoints = peak_local_max(corners, threshold_rel=0.2, exclude_border=True,
                                num_peaks=2000, min_distance=feature_width//2)
-    ys = keypoints[:,0]
-    xs = keypoints[:,1]
-
+    ys = keypoints[:, 0]
+    xs = keypoints[:, 1]
     return xs, ys
